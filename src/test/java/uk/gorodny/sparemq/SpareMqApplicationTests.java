3455f0d10f89df8fe47f85454b8616e79daac63a
Sergey Gorodnyuk <sergeygorodnyuk@yandex.ru>
2020-09-25 19:16:48 +0300

    Init commit

---
+++ src/test/java/uk/gorodny/sparemq/SpareMqApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gorodny.sparemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpareMqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
